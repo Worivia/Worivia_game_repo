3a16394c90bdd94e1a0ca30abf2db67b9bf649e9
Venc03 <vegi.dani1@gmail.com>
2023-05-19 08:31:09 +0200

    data.js feltoltese, index.html kitoltese(alap), uml abra hozzadva

---
+++ data.js
@@ -0,0 +1,25 @@
+export const KLISTA = ["Nevezze meg a világ legnagyobb szigetét!",
+ "Melyik ország termeli a legtöbb kávét a világon?",
+ "Hány grammyja van John Legendnek?",
+ "Mi az ég legfényesebb csillaga?",
+ "Milyen anyából készül a marcipán?",
+ "Hány felesége volt VIII. Henriknek?",
+ "Hogyan hívják, amikor egy üveg pezsgőt karddal nyitnak?",
+ "Milyen színű a zsiráf nyelve?",
+ "Melyik országban van a legtöbb automata egy főre jutva?",
+ "Mi volt az első játék, amelyet a televízióban hirdettek?"
+];
+
+export const VLISTA = ["Grönland",
+"Brazília",
+"Tizenegy",
+"Sirius",
+"Mandula",
+"6",
+"Sabering",
+"Kék fekete",
+"Japán",
+"Burgonyafő úr"
+];
+
+ //https://hu.jf-sjbrito.pt/110-best-fun-trivia-questions
